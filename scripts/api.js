ca2786f90863444aba1c495e09b1fd92c4d28c08
Scott Ian <sim2487@gmail.com>
2018-11-09 02:30:23 -0800

    added api function stubs

--- scripts/api.js
+++ scripts/api.js
@@ -1,11 +1,28 @@
 /*eslint-env jquery*/
 'use strict';
 const api = (function(){
+  const BASE_URL = 'https://thinkful-list-api.herokuapp.com/scott/bookmarks';
+
+  function getBookmarks(callbackfn) {
+
+  }
+
+  function createBookmark(name, successfn, errorfn) {
+
+  }
+
+  function deleteBookmark() {
+
+  }
+
   function apiTest(){
     console.log('hello from api');
   }
 
   return {
     apiTest,
+    getBookmarks,
+    createBookmark,
+    deleteBookmark,
   };
 }() );
