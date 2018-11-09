ac0c3cd1eb166a032a5541d4f8c4b48090613c75
Scott Ian <sim2487@gmail.com>
2018-11-09 02:54:16 -0800

    fleshed out api functions

--- scripts/api.js
+++ scripts/api.js
@@ -1,18 +1,34 @@
 /*eslint-env jquery*/
 'use strict';
+// eslint-disable-next-line no-unused-vars
 const api = (function(){
   const BASE_URL = 'https://thinkful-list-api.herokuapp.com/scott/bookmarks';
 
   function getBookmarks(callbackfn) {
-
+    $.getJSON(`${BASE_URL}`, callbackfn);
+    console.log('getting bookmarks from server');
   }
 
-  function createBookmark(name, successfn, errorfn) {
-
+  function createBookmark(newbookmarkObj, successfn, errorfn) {
+    console.log(newbookmarkObj);
+    $.ajax({
+      url: `${BASE_URL}`,
+      method: 'POST',
+      contentType: 'application/json',
+      data: newbookmarkObj,
+      success: successfn,
+      error: errorfn,
+    });
   }
 
-  function deleteBookmark() {
-
+  function deleteBookmark(id, successfn) {
+    console.log('deleteBookmark fired');
+    $.ajax({
+      url: `${BASE_URL}/${id}`,
+      method: 'POST',
+      contentType: 'application/json',
+      success: successfn,
+    });
   }
 
   function apiTest(){
